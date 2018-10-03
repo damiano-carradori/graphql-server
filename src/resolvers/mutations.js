3360aead6d50c7f5f64e16b0cca505e9d9214e2c
Damiano Carradori <damiano.carradori@gmail.com>
2018-10-03 17:28:48 +0200

    Updated the check for the presence of a field in the updateTask mutation

--- src/resolvers/mutations.js
+++ src/resolvers/mutations.js
@@ -5,6 +5,6 @@ module.exports = {
         let task = new Tasks({text});
         return await task.save();
     },
-    deleteTask: async (root, {id}) => await Tasks.deleteOne({ _id : id }),
-    updateTask: async (root, {id, text, done}) => await Tasks.updateOne({_id: id},{'$set': {...(text && {text}), ...(done && {done})}})
+    deleteTask: async (root, {id}) => await Tasks.deleteOne({ _id: id }),
+    updateTask: async (root, {id, text, done}) => await Tasks.updateOne({_id: id},{'$set': {...(text!==undefined && {text}), ...(done!==undefined && {done})}})
 };
